fb723acb1871b713539bf3ca32eb748eef7c8ef6
timpec <timpe96@hotmail.com>
2020-04-28 15:05:39 +0300

    querys with authentication token after login

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,11 +1,34 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
+import { TextField } from "@material-ui/core";
+import I18n from "../../components/Element/LanguageSwticher/I18n";
+import "./Profile.css";
+import {getUser} from '../../services/graphqlService';
 
 export default function Profile() {
+
+    const placeholder = async () => {
+        const id = localStorage.getItem('userid')
+        const user = await getUser(id)
+        console.log(user)
+    }
+
+
 
 
     return (
         <div>
             <h1>Profile</h1>
+
+            <div className="logout_btnContainer">
+                <button
+                type="button"
+                className="logout_btn"
+                onClick={() => placeholder()}>
+            {I18n.t("profile.logoutButton")}
+          </button>
         </div>
+        </div>
+
     )
 }
--- src/services/graphqlService.js
+++ src/services/graphqlService.js
@@ -1,11 +1,26 @@
-import { ApolloClient, HttpLink, InMemoryCache, gql } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache, gql, from } from '@apollo/client';
+import { setContext } from 'apollo-link-context';
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:3001/Graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
 
 const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    link: new HttpLink({
-      uri: 'http://localhost:3001/Graphql',
-    })
-  });
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
 
   export const getExampleRoutes = () => { 
 
@@ -198,6 +213,69 @@ const client = new ApolloClient({
       localStorage.setItem('userid', usr.id)
       localStorage.setItem('token', usr.token)
       return true
+    })
+    .catch(err => {
+      console.log(err)
+      return false
+    });
+  }
+
+  export const getUser = (id) => { 
+    return client.query({
+      query: gql`
+      {
+        user (id: "${id}") {
+          id 
+          username
+          email
+          address
+          intrests
+          friends {
+            id
+            email
+            intrests
+            reservations {
+              id
+              name {fi en}
+              event_dates {
+                starting_day
+                ending_day
+              }
+            }
+          }
+          reservations {
+            id
+            name {fi en}
+            source_type {id name}
+            info_url
+            location {
+              lat
+              lon
+              address {
+                locality
+                street_address
+                postal_code
+              }
+            }
+            description {
+              intro
+              body
+              images {url}
+            }
+            tags {id name}
+            event_dates {
+              starting_day
+              ending_day
+              additional_description
+              weather {temp}
+            }
+          }
+        }
+      }
+      `
+    })
+    .then(result => {
+      return(result.data.user)
     })
     .catch(err => {
       console.log(err)
